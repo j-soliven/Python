5a9ada75f1b6d8ad6143f948cf634aec0aad5f77
j-soliven <46631911+j-soliven@users.noreply.github.com>
2019-05-01 17:15:26 -0400

    New Progress - 5/1

--- Chapter 6 - Dictionaries/favorite_languages.py
+++ Chapter 6 - Dictionaries/favorite_languages.py
@@ -3,6 +3,18 @@ favorite_languages = {
     'bob' : 'c++',
     'jen' : 'java'
 }
+
 print("John's favorite language is " + 
     favorite_languages['john'].capitalize() +
     ".")
+
+# looping through key-value pairs
+for key, value in favorite_languages.items():
+    print("Key: " + key)
+    print("Value: " + value + "\n")
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + language.title() + "\n")
+
+for name in favorite_languages:
+    print(name.title())
