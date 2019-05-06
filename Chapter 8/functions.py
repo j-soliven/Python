4e24c6101f7856fd7223b0f4406bf33c725a2ab8
solivenj <soliven.j@husky.neu.edu>
2019-05-06 00:10:19 -0400

    progress 5/5

--- Chapter 8/functions.py
+++ Chapter 8/functions.py
@@ -12,3 +12,22 @@ def greet_user(username):
     print("Hello " + username.title())
 
 greet_user("john")
+
+# keyword arguments
+# position does not matter
+
+def describe_pet(pet_name, animal_type):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name)
+
+describe_pet(pet_name = 'Sparky', animal_type = 'dog')
+describe_pet(animal_type = 'dog', pet_name = 'Sparky')
+
+
+# default value functions
+def describe_pet2(petname, animal_type='dog'):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + petname.title() + ".")
+
+describe_pet2('sparky')
+describe_pet2('sparky', 'cat')
